ba4f3ff7d458c4b9705b845c89cb2ecf793e5edf
CardoPixel <cardo.pixelado@gmail.com>
2024-06-20 14:26:19 -0300

    Initial commit

---
+++ test/stripe-webhook-with-appsync.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StripeWebhookWithAppsync from '../lib/stripe-webhook-with-appsync-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stripe-webhook-with-appsync-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StripeWebhookWithAppsync.StripeWebhookWithAppsyncStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
